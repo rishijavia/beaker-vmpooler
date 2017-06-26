fc0e92e362eb9b8a6cc5f119dbfcace99731157d
Rishi Javia <rishi.javia.3895@gmail.com>
2017-06-26 16:10:20 -0700

    Update fixtures load path

--- test-options.rb
+++ test-options.rb
@@ -1,6 +1,5 @@
 beaker_gem_spec = Gem::Specification.find_by_name('beaker')
 beaker_gem_dir = beaker_gem_spec.gem_dir
-beaker_test_base_dir = File.join(beaker_gem_dir, 'acceptance/tests/base')
 load_lib = File.join(beaker_gem_dir, 'acceptance/lib')
-load_fixtures = File.join(beaker_gem_dir, 'acceptance/fixtures/module/lib')
+load_fixtures = File.join(beaker_gem_dir, 'acceptance/fixtures')
 {:load_path=>[load_lib, load_fixtures]}
